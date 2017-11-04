c909c7e13b8a12acc391c7775a0b4e13aa8d7b7c
Tillman Rossa <tillman.rossa@hs-augsburg.de>
2017-11-04 12:58:21 +0100

    Started working on init

--- Source/Master/data.py
+++ Source/Master/data.py
@@ -1,10 +1,5 @@
 ###Data Module
 
-import Source.Master.logic
-
 planet_positions = []
 planet_speeds = []
 planet_weights = []
-
-def foo():
-    print('Hello World')
--- Source/Master/logic.py
+++ Source/Master/logic.py
@@ -1,10 +1,16 @@
 #
 from random import randint
-import Source.Master.data
+import math
+import data
 
 def initPlanets():
     number_of_planets = randint(2, 5)
     for _ in range(number_of_planets):
-        planet_weight = randint(1, 9) * 10^24
+        planet_weight = randint(1, 9) * math.pow(10, 24)
+        data.planet_weights.append(planet_weight)
+
+    for planet in data.planet_weights:
+        print(planet)
 
 
+initPlanets()
