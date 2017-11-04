f979dca3b848e3fbca81fa41a0db6ef0481a4b97
Tillman Rossa <tillman.rossa@hs-augsburg.de>
2017-11-04 12:46:30 +0100

    Added more folders

---
+++ Source/Master/data.py
@@ -0,0 +1,10 @@
+###Data Module
+
+import Source.Master.logic
+
+planet_positions = []
+planet_speeds = []
+planet_weights = []
+
+def foo():
+    print('Hello World')
---
+++ Source/Master/logic.py
@@ -0,0 +1,10 @@
+#
+from random import randint
+import Source.Master.data
+
+def initPlanets():
+    number_of_planets = randint(2, 5)
+    for _ in range(number_of_planets):
+        planet_weight = randint(1, 9) * 10^24
+
+
